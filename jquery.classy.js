34b44592bebde36f1353e643b8c56a1e82f3ea0e
adamcoulombe <adamcoulombe187@hotmail.com>
2012-12-14 16:40:09 -0500

    many updates

--- jquery.classy.js
+++ jquery.classy.js
@@ -1,12 +1,48 @@
+/*! jQuery.classy() - v0.0.1 - 2012-12-14
+* Copyright (c) 2012 Adam Coulombe; Licensed MIT, GPL */
 (function($){
+	$.extend({
+	
+		getBrowserVendorPrefix : function(lowercaseFormat){
+			
+			var regex = /^(Moz|Webkit|Khtml|O|ms|Icab)(?=[A-Z])/;
+			var el = document.getElementsByTagName('script')[0];
+		
+			for(var prop in el.style)
+			{
+				if(regex.test(prop))
+				{
+					prefix = prop.match(regex)[0].toLowerCase();
+					if(!lowercaseFormat){
+						if(prefix=='ms'){
+							prefix = 'MS'
+						}
+						if(prefix=='moz'){
+							prefix = 'Moz'
+						}
+					}
+					return prefix;
+				}
+			}
+			if('WebkitOpacity' in el.style) {
+				return 'webkit';
+			}
+			if('KhtmlOpacity' in el.style) {
+				return 'khtml';
+			}
+		
+			return '';
+		}	
+	});
 	var $bind = $.fn.bind;
     $.fn.bind = function()
     {
 		if(typeof arguments[0] === 'string' ){
-			arguments[0] = arguments[0].replace('transitionend', 'transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd');
-			arguments[0] = arguments[0].replace('animationstart', 'animationstart webkitAnimationStart MSAnimationStart oAnimationStart');
-			arguments[0] = arguments[0].replace('animationiteration', 'animationiteration webkitAnimationIteration MSAnimationIteration oAnimationIteration');
-			arguments[0] = arguments[0].replace('animationend', 'animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd');
+			browserVendorPrefix = $.getBrowserVendorPrefix();
+			arguments[0] = arguments[0].replace('transitionend', 'transitionend '+ browserVendorPrefix +'TransitionEnd');
+			arguments[0] = arguments[0].replace('animationstart', 'animationstart '+ browserVendorPrefix +'AnimationStart');
+			arguments[0] = arguments[0].replace('animationiteration', 'animationiteration '+ browserVendorPrefix +'AnimationIteration');
+			arguments[0] = arguments[0].replace('animationend', 'animationend '+ browserVendorPrefix +'AnimationEnd');
 		}
   	    var ret = $bind.apply(this, arguments);
         return ret;
@@ -15,107 +51,149 @@
     $.fn.unbind = function()
     {
 		if(typeof arguments[0] === 'string' ){
-			arguments[0] = arguments[0].replace('transitionend', 'transitionend webkitTransitionEnd MSTransitionEnd oTransitionEnd');
-			arguments[0] = arguments[0].replace('animationstart', 'animationstart webkitAnimationStart MSAnimationStart oAnimationStart');
-			arguments[0] = arguments[0].replace('animationiteration', 'animationiteration webkitAnimationIteration MSAnimationIteration oAnimationIteration');
-			arguments[0] = arguments[0].replace('animationend', 'animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd');
+			browserVendorPrefix = $.getBrowserVendorPrefix();
+			arguments[0] = arguments[0].replace('transitionend', 'transitionend '+ browserVendorPrefix +'TransitionEnd');
+			arguments[0] = arguments[0].replace('animationstart', 'animationstart '+ browserVendorPrefix +'AnimationStart');
+			arguments[0] = arguments[0].replace('animationiteration', 'animationiteration '+ browserVendorPrefix +'AnimationIteration');
+			arguments[0] = arguments[0].replace('animationend', 'animationend '+ browserVendorPrefix +'AnimationEnd');
 		}
   	    var ret = $unbind.apply(this, arguments);
         return ret;
     };
 	$.fn.extend({
 	 	classy : function() {
-			var classyClass;
-			var callback;
 			var options = {};
+			var callback = null;
 			
 			if(typeof arguments[0] === 'string' ){
-				classyClass = arguments[0];
-
-				if(typeof arguments[1] === 'function'){
-					callback = arguments[1];
-				}else {
-					callback = arguments[2] || function(){};
-					if(typeof arguments[1] === 'object'){ options =  arguments[1]; }
+				options = {add:arguments[0],end:{}};
+			}else if(typeof arguments[0] === 'object' ){
+				options = arguments[0];
+				options.end = arguments[0].end || {};
+				callback = options.end.call;
+			}
+			
+			if(typeof arguments[1] === 'function'){
+				if(callback){
+					options.end.call = function(){
+						callback();
+						arguments[1]();
+					}
+				}else{
+					options.end.call = arguments[1];
 				}
-				
-			}else{
-				classyClass='';
+			}else {
+				if(arguments[2]){
+					if(callback){
+						options.end.call = function(){
+							callback();
+							arguments[2]();
+						}
+					}else{
+						options.end.call = arguments[2];
+					}
+				}
+				if(typeof arguments[1] === 'object'){
+					if(options.end.add || options.end.remove || options.end.call){
+						options.end.add = options.end.add ? options.end.add + arguments[1].add : arguments[1].add;
+						options.end.remove = options.end.remove ? options.end.remove + arguments[1].remove : arguments[1].remove;
+						if(typeof options.end.call === 'function' &&  typeof arguments[1].call === 'function' ){
+							var oldCallback = options.end.call;
+							var addedCallback = arguments[1].call;
+							var newCallback = function(){
+								addedCallback();
+								oldCallback();
+							}
+							
+							options.end.call = newCallback;
+						}
+					}else{
+						options.end = arguments.call[1];
+					}
+				}
 			}
 			
 			var defaults = {
-				now:{
-					add:null,
-					remove:null,
-					call:function(){ }
-				},
+				add:null,
+				remove:null,
+				call:null,
 				start:{
 					add:null,
 					remove:null,
-					call:function(){ },
+					call:null,
 					eventPersist:null
 				},
 				end:{
 					add:null,
 					remove:null,
-					call:function(){ },
+					call:null,
 					eventPersist:null
 				},
 				iteration:{
 					add:null,
 					remove:null,
-					call:function(){ },
+					call:null,
 					eventPersist:true
 				},
-				type:'animation'			
+				type: null	
 			};
 			var options = $.extend(defaults, options);
-			var endEvent = options.type + 'end';
-			
-			//console.log(endEvent);
+
 			return this.each(function() {
 				var $this = $(this);
-				
-				//console.log('classyClass: '+ classyClass);
-				$this.addClass(classyClass);
-				
-				var onNow = function(){
-					if(typeof options.now.add == 'string'){ $this.addClass(options.now.add) }
-					if(typeof options.now.remove == 'string'){ $this.removeClass(options.now.remove) }
-					if(typeof options.now.call == 'function'){ options.now.call(); }
+				if(!options.type){
+					if(parseFloat($this.css('-'+$.getBrowserVendorPrefix()+'-transition-duration')) > 0 || parseFloat($this.css('transition-duration')) > 0 ){
+						options.type = 'transition'
+					}else{
+						options.type = 'animation'
+					}
 				}
+				console.log(options.type);
+				var endEvent = options.type + 'end';
+				
+				var onInit = function(){
+					if(typeof options.add == 'string'){ $this.addClass(options.add);  }
+					if(typeof options.remove == 'string'){ $this.removeClass(options.remove);  }
+					if(typeof options.call == 'function'){ options.call(); }
+				}
+
 				var onEnd = function(){
 					if(!options.end.eventPersist){ $this.unbind(endEvent); }
-					if(typeof options.end.add == 'string'){ $this.addClass(options.end.add) }
-					if(typeof options.end.remove == 'string'){ $this.removeClass(options.end.remove) }
+					if(typeof options.end.add == 'string'){ $this.addClass(options.end.add); }
+					if(typeof options.end.remove == 'string'){ $this.removeClass(options.end.remove);}
 					if(typeof options.end.call == 'function'){ options.end.call(); }
-					callback();
 				}
 
-				
-				onNow();
-				//console.log('binding to '+endEvent);
-				$this.bind(endEvent,onEnd);
+				if(typeof options.add == 'string' || typeof options.remove == 'string' || typeof options.call == 'function'){
+					onInit();
+				}
+				if(typeof options.end.add == 'string' || typeof options.end.remove == 'string' || typeof options.end.call == 'function'){
+					$this.bind(endEvent,onEnd);
+				}
 
 				if(options.type=='animation'){
+					
 					var onStart = function(){
 						if(!options.start.eventPersist){ $this.unbind('animationstart'); }
-						if(typeof options.start.add == 'string'){ $this.addClass(options.start.add) }
-						if(typeof options.start.remove == 'string'){ $this.removeClass(options.start.remove) }
+						if(typeof options.start.add == 'string'){ $this.addClass(options.start.add);  }
+						if(typeof options.start.remove == 'string'){ $this.removeClass(options.start.remove);  }
 						if(typeof options.start.call == 'function'){ options.start.call(); }
 					}
-					$this.bind('animationstart',onStart);
+					if(typeof options.start.add == 'string' || typeof options.start.remove == 'string' || typeof options.start.call == 'function'){
+						$this.bind('animationstart',onStart);
+					}
+					
 					var onIteration = function(){
 						if(!options.iteration.eventPersist){ $this.unbind('animationiteration'); }
-						if(typeof options.iteration.add == 'string'){ $this.addClass(options.iteration.add) }
-						if(typeof options.iteration.remove == 'string'){ $this.removeClass(options.iteration.remove) }
-						if(typeof options.iteration.call == 'function'){ options.iteration.call(); }
+						if(typeof options.iteration.add == 'string'){ $this.addClass(options.iteration.add);  }
+						if(typeof options.iteration.remove == 'string'){ $this.removeClass(options.iteration.remove);  }
+						if(typeof options.iteration.call == 'function'){ options.iteration.call();  }
 					}
-					$this.bind('animationiteration',onIteration);
+					if(typeof options.start.add == 'string' || typeof options.start.remove == 'string' || typeof options.start.call == 'function'){
+						$this.bind('animationiteration',onIteration);
+					}
 				}
 			});
 		}
-		
-	
 	});
 })(jQuery);
